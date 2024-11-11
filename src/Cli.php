027c4c0818ce6b88c9646962026f3fa4bb91b0e9
bakhytbek <bahitbek.kz@mail.ru>
2024-11-11 14:53:35 +0500

    Приветствие

---
+++ src/Cli.php
@@ -0,0 +1,11 @@
+<?php
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcome(){
+ line('Welcome to the Brain Game!');
+ $name = prompt('May I have your name?');
+ line("Hello, %s!", $name);
+}
